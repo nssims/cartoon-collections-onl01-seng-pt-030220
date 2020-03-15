d5d423e722fca14ecd042b1e50e83ded71e92164
Nicole Sims <nssims105@gmail.com>
2020-03-15 01:13:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+ddef roll_call_dwarves(array)
+  array.collect.with_index do |name, index|
+    name_place = index + 1
+    puts "#{name_place} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |word| 
+    word.capitalize + "!"
+  end 
+end 
+
+def long_planeteer_calls(array)
+  array.any?{|word| word.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|cheese| cheese_types.include?(cheese)}
 end
